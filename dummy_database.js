418cd3d722656b4ad086d551e35e526f4295f3e9
Tilak Sharma <tilak.sharma.mail@gmail.com>
2019-07-20 21:42:07 +0530

    signin and register end point

---
+++ dummy_database.js
@@ -0,0 +1,38 @@
+const database = {
+    users : [
+        {
+            id : '1',
+            name : 'abc',
+            email : 'abc',
+            password : 'abc',
+            entries : 0,
+            joined : new Date(),
+        },
+        {
+            id : '2',
+            name : 'abc',
+            email : 'abc',
+            password : 'abc',
+            entries : 0,
+            joined : new Date(),
+        },
+        {
+            id : '3',
+            name : 'abc',
+            email : 'abc',
+            password : 'abc',
+            entries : 0,
+            joined : new Date(),
+        },
+        {
+            id : '4',
+            name : 'abc',
+            email : 'abc',
+            password : 'abc',
+            entries : 0,
+            joined : new Date(),
+        }
+    ]
+}
+
+module.exports = database;
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 const express = require('express');
+const database = require('./dummy_database');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (request, response)=>{
-    response.send('This is working');
+    response.send('This is working');   
 })
 
-app.listen(3001, ()=>{
-    console.log('Listening to localhost:3001....');
+// SIGN IN
+app.post('/signin', (request, response)=>{
+    if(request.body.email === database.users[0].email &&
+        request.body.password === database.users[0].password){
+            response.json("signing in");
+        }
+    else{
+        response.status(400).send("error signing in");
+    }
 })
+
+// REGISTER
+app.post('/register', (request, response)=>{
+    const { name, email ,password} = request.body;
+    database.users.push({
+
+        id : '5',
+        name : name,
+        email : email,
+        password : password,
+        entries : 0,
+        joined : new Date()
+        
+    })
+    response.json(database.users[database.users.length -1]);
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening to localhost:3000....');
+})
+
+
+/**
+ *      / => this is working
+ *      /signin => POST = success/fail
+ *      /register => POST = user
+ *      /profile:id => GET = user
+ *      /image  => PUT = user  this endpoint if for updating rank based on inputs
+ */
